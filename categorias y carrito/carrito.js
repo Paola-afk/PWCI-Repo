f90db983f6214dde1aab2ed35e38f2a0bb9ace86
Paola-afk <paola.carvajalguevara@gmail.com>
2024-09-21 00:07:44 -0600

    Propuesta de APIs

--- categorias y carrito/carrito.js
+++ categorias y carrito/carrito.js
@@ -1,3 +1,22 @@
+//Acciones al picar la foto del usuario en header
+document.addEventListener("DOMContentLoaded", function() {
+    const profileMenuToggle = document.getElementById('profileMenuToggle');
+    const profileMenu = document.getElementById('profileMenu');
+
+    profileMenuToggle.addEventListener('click', function() {
+        const isVisible = profileMenu.style.display === 'block';
+        profileMenu.style.display = isVisible ? 'none' : 'block';
+    });
+
+    // Ocultar el menú si se hace clic fuera de él
+    document.addEventListener('click', function(event) {
+        if (!profileMenuToggle.contains(event.target) && !profileMenu.contains(event.target)) {
+            profileMenu.style.display = 'none';
+        }
+    });
+});
+
+
 
 const paymentMethod = document.getElementById('payment-method');
 const formTarjeta = document.getElementById('form-tarjeta');
